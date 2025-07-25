5e113b31d03c632f5af8a9541b6e8d13eb2f5fd8
reethumthomas <thomasreethu694@gmail.com>
2025-07-25 18:34:07 +0530

    updated datetime formatting

--- utils/screenshot_utils.py
+++ utils/screenshot_utils.py
@@ -6,7 +6,7 @@ def take_screenshot(driver, test_name):
     folder = "screenshots"
     os.makedirs(folder, exist_ok=True)
 
-    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
     filename = f"{test_name}_{timestamp}.png"
     path = os.path.join(folder, filename)
 
